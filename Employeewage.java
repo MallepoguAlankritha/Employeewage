838049705949507a39b5e523e64178c12d6a3e70
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-22 10:37:20 +0530

    Employeeattendance

--- Employeewage.java
+++ Employeewage.java
@@ -1,10 +1,12 @@
 package com.bridgelabz;
 
 public class Employeewage {
-
-	public static void main(String[] args) {
-		System.out.println("welcome to employee wage computation");
-
+		public static void main(String[] args){
+		  int IS_FULL_TIME = 1;
+		  double empCheck = Math.floor(Math.random() * 10) % 2;
+		   if(empCheck == IS_FULL_TIME)
+		System.out.println("Employee is present");
+		else
+		System.out.println("Employee is Absent");
+		}
 	}
-
-}
